a79b8eb629c995d714e1e79f5029ee32874eda9b
andrebraga7 <andrestrevisan@gmail.com>
2023-04-27 21:15:50 +0000

    Add icons to mobile navbar and style

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,34 +5,72 @@ import Navbar from "react-bootstrap/Navbar";
 import styles from "../styles/NavBar.module.css";
 
 const mobileNavBar = (
-  <Navbar expand="md">
+  <>
+    <Navbar expand="md">
+      <Container className="m-2">
+        <Navbar.Brand className={styles.NavBrand} href="#">
+          Quotme
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <Nav.Link href="#" className={styles.NavLink}>
+              Login
+            </Nav.Link>
+            <Nav.Link href="#" className={styles.NavLink}>
+              Sign Up
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
     <Container>
-      <Navbar.Brand className={styles.NavBrand} href="#">
-        Quotme
-      </Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="ms-auto">
-          <Nav.Link href="#" className={styles.NavLink}>Login</Nav.Link>
-          <Nav.Link href="#" className={styles.NavLink}>Sign Up</Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
+      <Nav className="justify-content-between mx-3">
+        <Nav.Link className={`d-flex flex-column ${styles.NavBarMobile}`}>
+          <i className="fas fa-home"></i>
+          <span>Home</span>
+        </Nav.Link>
+        <Nav.Link className={`d-flex flex-column ${styles.NavBarMobile}`}>
+          <i className="fa-regular fa-compass"></i>
+          <span>Discover</span>
+        </Nav.Link>
+        <Nav.Link className={`d-flex flex-column ${styles.NavBarMobile}`}>
+          <i className="fa-solid fa-square-plus"></i>
+          <span>Quote</span>
+        </Nav.Link>
+        <Nav.Link className={`d-flex flex-column ${styles.NavBarMobile}`}>
+          <i className="fa-solid fa-pen-fancy"></i>
+          <span>Authors</span>
+        </Nav.Link>
+        <Nav.Link className={`d-flex flex-column ${styles.NavBarMobile}`}>
+          <i className="fa-regular fa-user"></i>
+          <span>Profile</span>
+        </Nav.Link>
+      </Nav>
     </Container>
-  </Navbar>
+  </>
 );
 
 const largeScreenNavBar = (
   <Navbar>
     <Container>
-      <Navbar.Brand className={styles.NavBrand}>
-        Quotme
-      </Navbar.Brand>
+      <Navbar.Brand className={styles.NavBrand}>Quotme</Navbar.Brand>
       <Nav className="ms-auto">
-        <Nav.Link href="#" className={styles.NavLink}>Home</Nav.Link>
-        <Nav.Link href="#" className={styles.NavLink}>Discover</Nav.Link>
-        <Nav.Link href="#" className={styles.NavLink}>Author</Nav.Link>
-        <Nav.Link href="#" className={styles.NavLink}>Login</Nav.Link>
-        <Nav.Link href="#" className={styles.NavLink}>Sign Up</Nav.Link>
+        <Nav.Link href="#" className={styles.NavLink}>
+          Home
+        </Nav.Link>
+        <Nav.Link href="#" className={styles.NavLink}>
+          Discover
+        </Nav.Link>
+        <Nav.Link href="#" className={styles.NavLink}>
+          Author
+        </Nav.Link>
+        <Nav.Link href="#" className={styles.NavLink}>
+          Login
+        </Nav.Link>
+        <Nav.Link href="#" className={styles.NavLink}>
+          Sign Up
+        </Nav.Link>
       </Nav>
     </Container>
   </Navbar>
