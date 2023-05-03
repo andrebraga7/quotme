741fb550bc278e8c945e469f67c063f3a99d83eb
andrebraga7 <andrestrevisan@gmail.com>
2023-05-03 18:41:05 +0000

    Add handleSubmit to the comment create form and post data

--- src/pages/comments/CommentCreateForm.js
+++ src/pages/comments/CommentCreateForm.js
@@ -2,11 +2,12 @@ import React, { useState } from "react";
 import styles from "../../styles/CommentCreateForm.module.css";
 import btnStyles from "../../styles/Button.module.css";
 import Avatar from "../../components/Avatar";
+import { Link } from "react-router-dom";
+import { axiosRes } from "../../api/axiosDefaults";
 
 // React Bootstrap imports
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
-import FloatingLabel from "react-bootstrap/FloatingLabel";
 import Card from "react-bootstrap/Card";
 import InputGroup from "react-bootstrap/InputGroup";
 
@@ -18,12 +19,39 @@ function CommentCreateForm(props) {
     setContent(event.target.value);
   };
 
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const { data } = await axiosRes.post("/comments/", {
+        content,
+        quote,
+      });
+      setComments((prevComments) => ({
+        ...prevComments,
+        results: [data, ...prevComments.results],
+      }));
+      setQuote((prevQuote) => ({
+        results: [
+          {
+            ...prevQuote.results[0],
+            comments_count: prevQuote.results[0].comments_count + 1,
+          },
+        ],
+      }));
+      setContent("");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <Card body className={styles.Comment}>
-      <Form>
+      <Form onSubmit={handleSubmit}>
         <Form.Group className={styles.Group}>
           <InputGroup>
-            <Avatar src={profileImage} />
+            <Link to={`/profiles/${profile_id}`}>
+              <Avatar src={profileImage} />
+            </Link>
             <Form.Control
               className={styles.Input}
               rows={2}
@@ -35,7 +63,13 @@ function CommentCreateForm(props) {
             />
           </InputGroup>
         </Form.Group>
-        <Button className={`${btnStyles.ButtonComment} ${btnStyles.Dark} d-flex ms-auto`}>Send</Button>
+        <Button
+          className={`${btnStyles.ButtonComment} ${btnStyles.Dark} d-flex ms-auto`}
+          disabled={!content.trim()}
+          type="submit"
+        >
+          Send
+        </Button>
       </Form>
     </Card>
   );
