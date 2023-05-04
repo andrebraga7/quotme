11b50f2d5db79c409b9514bb0dbe8383d21054c0
andrebraga7 <andrestrevisan@gmail.com>
2023-05-04 17:50:54 +0000

    Create the handleFollow function

--- src/contexts/ProfileDataContext.js
+++ src/contexts/ProfileDataContext.js
@@ -1,4 +1,5 @@
 import { createContext, useContext, useState } from "react";
+import { axiosRes } from "../api/axiosDefaults";
 
 export const ProfileDataContext = createContext();
 export const SetProfileDataContext = createContext();
@@ -8,6 +9,43 @@ export const useSetProfileData = () => useContext(SetProfileDataContext);
 
 export const ProfileDataProvider = ({ children }) => {
   const [profileData, setProfileData] = useState({});
+
+  const handleFollow = async (clickedProfile) => {
+    try {
+      const { data } = await axiosRes.post("/followers/", {
+        followed: clickedProfile.id,
+      });
+      setProfileData((prevState) => ({
+        ...prevState,
+        followers_count: prevState.followers_count + 1,
+        following_id: data.id,
+      }));
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const handleUnfollow = async (clickedProfile) => {
+    try {
+      await axiosRes.delete(`/followers/${clickedProfile.following_id}`);
+      setProfileData((prevState) => ({
+        ...prevState,
+        pageProfile: {
+          results: prevState.pageProfile.results.map((profile) =>
+            unfollowHelper(profile, clickedProfile)
+          ),
+        },
+        popularProfiles: {
+          ...prevState.popularProfiles,
+          results: prevState.popularProfiles.results.map((profile) =>
+            unfollowHelper(profile, clickedProfile)
+          ),
+        },
+      }));
+    } catch (err) {
+      // console.log(err);
+    }
+  };
 
   return (
     <ProfileDataContext.Provider value={profileData}>
