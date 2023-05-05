4c3a12870e8eb19a999cb3f3391e0dc705325957
andrebraga7 <andrestrevisan@gmail.com>
2023-05-05 20:10:00 +0000

    Add a searchbar to the authors page

--- src/pages/authors/AuthorsPage.js
+++ src/pages/authors/AuthorsPage.js
@@ -3,21 +3,22 @@ import styles from "../../styles/AuthorsPage.module.css";
 import { axiosReq } from "../../api/axiosDefaults";
 import Asset from "../../components/Asset";
 import Author from "./Author";
-import InfiniteScroll from "react-infinite-scroll-component";
-import { fetchMoreData } from "../../utils/utils";
 
 // React Bootstrap imports
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import Form from "react-bootstrap/Form";
+import InputGroup from "react-bootstrap/InputGroup";
 
 function AuthorsPage() {
   const [authors, setAuthors] = useState({ results: [] });
+  const [query, setQuery] = useState("");
   const [hasLoaded, setHasLoaded] = useState(false);
 
   useEffect(() => {
     const fetchAuthors = async () => {
       try {
-        const { data } = await axiosReq.get("/authors/");
+        const { data } = await axiosReq.get(`/authors/?search=${query}`);
         setAuthors(data);
         setHasLoaded(true);
       } catch (error) {
@@ -26,8 +27,13 @@ function AuthorsPage() {
     };
 
     setHasLoaded(false);
-    fetchAuthors();
-  }, [setAuthors]);
+    const timer = setTimeout(() => {
+      fetchAuthors();
+    }, 1000);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [setAuthors, query]);
 
   return (
     <>
@@ -39,6 +45,27 @@ function AuthorsPage() {
       </Row>
       <Row className={styles.Row}>
         <Col md={8} className="mx-auto mt-5">
+          <Row className="justify-content-center">
+            <Col md={8}>
+              <Form
+                className="mb-4"
+                onSubmit={(event) => event.preventDefault()}
+              >
+                <InputGroup>
+                  <InputGroup.Text className={styles.SearchIcon}>
+                    <i className="fa-solid fa-magnifying-glass"></i>
+                  </InputGroup.Text>
+                  <Form.Control
+                    className={styles.SearchInput}
+                    type="text"
+                    placeholder="Search authors"
+                    value={query}
+                    onChange={(event) => setQuery(event.target.value)}
+                  />
+                </InputGroup>
+              </Form>
+            </Col>
+          </Row>
           {hasLoaded ? (
             <>
               {authors.results.length ? (
