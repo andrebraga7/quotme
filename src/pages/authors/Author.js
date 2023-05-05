bfa29ef2c5a866eb76d95235f2a7cfe5f703d7f4
andrebraga7 <andrestrevisan@gmail.com>
2023-05-05 19:58:08 +0000

    Style Author component

--- src/pages/authors/Author.js
+++ src/pages/authors/Author.js
@@ -1,14 +1,18 @@
 import React from "react";
+import styles from "../../styles/Author.module.css";
 import Card from "react-bootstrap/Card";
+import { Link } from "react-router-dom";
 
 function Author(props) {
-  const { name, quotes_count } = props;
+  const { id, name, quotes_count } = props;
 
   return (
-    <Card body>
-      <span>{name}</span>
-      <span>{quotes_count}</span>
-    </Card>
+    <Link to={`/authors/${id}`}>
+      <Card body className={`${styles.Content} align-items-center flex-row`}>
+        <h3 className={styles.Name}>{name}</h3>
+        <p>Number of quotes: {quotes_count}</p>
+      </Card>
+    </Link>
   );
 }
 
--- src/pages/authors/AuthorsPage.js
+++ src/pages/authors/AuthorsPage.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import styles from "../../styles/AuthorsPage.module.css";
 import { axiosReq } from "../../api/axiosDefaults";
 import Asset from "../../components/Asset";
 import Author from "./Author";
@@ -29,29 +30,33 @@ function AuthorsPage() {
   }, [setAuthors]);
 
   return (
-    <Row>
-      <Col md={6} className="mx-auto my-auto">
-        {hasLoaded ? (
-          <>
-            {authors.results.length ? (
-              <InfiniteScroll
-                children={authors.results.map((author) => (
-                  <Author key={author.is} {...author} />
-                ))}
-                dataLength={authors.results.length}
-                loader={<Asset spinner />}
-                hasMore={!!authors.next}
-                next={() => fetchMoreData(authors, setAuthors)}
-              />
-            ) : (
-              <p>No authors?</p>
-            )}
-          </>
-        ) : (
-          <Asset spinner />
-        )}
-      </Col>
-    </Row>
+    <>
+      <Row className={styles.Header}>
+        <Col className="mx-auto" md={10}>
+          <h1>Popular Authors</h1>
+          <p>These are the top 10 most popular authors.</p>
+        </Col>
+      </Row>
+      <Row className={styles.Row}>
+        <Col md={8} className="mx-auto mt-5">
+          {hasLoaded ? (
+            <>
+              {authors.results.length ? (
+                <div className="d-flex flex-wrap justify-content-center">
+                  {authors.results.map((author) => (
+                    <Author key={author.is} {...author} />
+                  ))}
+                </div>
+              ) : (
+                <p>No authors?</p>
+              )}
+            </>
+          ) : (
+            <Asset spinner />
+          )}
+        </Col>
+      </Row>
+    </>
   );
 }
 
